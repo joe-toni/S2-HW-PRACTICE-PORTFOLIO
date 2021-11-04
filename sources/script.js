3834f7856f67757c60fe3c808d938b1075134e49
joe-toni <joefaburrieta@gmail.com>
2021-11-04 05:00:48 -0700

    touched up the styling and added a new style sheet for mobile focused display of the page

--- sources/script.js
+++ sources/script.js
@@ -3,14 +3,18 @@ const dayPlanner = document.querySelector(".day-planner");
 const weatherDash = document.querySelector(".weather-dashboard");
 const Info = document.querySelectorAll(".project-info");
 
-console.log(firstProject);
-console.log(dayPlanner);
-console.log(weatherDash);
+const bio = document.querySelector(".bio");
+const aboutMe = document.querySelector(".aboutMe");
 
-var focused = false;
 
 function focusProject(element, info)
 {
     element.classList.toggle("focused");
     info.classList.toggle("hideInfo");
+}
+
+function expand()
+{
+    bio.classList.toggle("mini");
+    aboutMe.classList.toggle("hideMe");
 }
