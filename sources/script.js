6d7868981a76cca5777ca70403dddc1e27a950c9
joe-toni <joefaburrieta@gmail.com>
2021-11-06 04:47:43 -0700

    Cleaned up the code were ever noticable, removed unused files and added lightly descriptive commenting

--- sources/script.js
+++ sources/script.js
@@ -1,3 +1,4 @@
+//Started by selecting all the elements from the dom that will be manipulated in our fucntions
 const firstProject = document.querySelector(".first-project");
 const dayPlanner = document.querySelector(".day-planner");
 const weatherDash = document.querySelector(".weather-dashboard");
@@ -6,13 +7,15 @@ const Info = document.querySelectorAll(".project-info");
 const bio = document.querySelector(".bio");
 const aboutMe = document.querySelector(".aboutMe");
 
-
+//This function adjusts the display of the project container to bring focus on one project in particular
+//Through the toggling of class names.
 function focusProject(element, info)
 {
     element.classList.toggle("focused");
     info.classList.toggle("hideInfo");
 }
-
+//This function performs similarly to the previous one but is intended to be used with the bio
+//section of the page.
 function expand()
 {
     bio.classList.toggle("mini");
